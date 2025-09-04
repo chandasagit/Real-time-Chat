58d0b4a19e867da0051a076f4b2991ea3d1080c9
Chandasa Puppala <124992655+chandasagit@users.noreply.github.com>
2025-09-05 00:03:09 +0530

    Create chatlist.jsx

---
+++ chatlist.jsx
@@ -0,0 +1,11 @@
+export default function ChatList() {
+  return (
+    <div className="chat-list">
+      <h3>Chats</h3>
+      <ul>
+        <li>General</li>
+        <li>Random</li>
+      </ul>
+    </div>
+  );
+}
